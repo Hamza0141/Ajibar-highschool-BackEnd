4c9dcaf5e8249e35fb4175caa06bd946afe32d82
Hamza0141 <131574624+Hamza0141@users.noreply.github.com>
2024-02-14 19:11:37 -0600

    cors updated

--- server.js
+++ server.js
@@ -9,20 +9,16 @@ const questionRouter = require("./server/api/questions/question.router");
 const answerRouter = require("./server/api/answers/answer.router");
 
 
-    app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-    });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+const corsOptions = {
+  origin:"https://ajibar-school-forum.pages.dev",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-
-// const corsOptions = {
-//     origin: "https://ajibar-school-forum.pages.dev",
-//     optionsSuccessStatus: 200,
-// };
-
-
-// app.use(cors(corsOptions));
-// app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use("/api/users", userRouter);
